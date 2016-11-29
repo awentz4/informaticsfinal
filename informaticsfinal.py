d94f34d9f4cef9bfbc9c5c39f3ba020b2ee24505
Andrew Wentz <awentz4@gmail.com>
2016-11-29 14:56:29 -0600

    Updated python file, added original data csv
    Organized code, added comments for easy reading

--- informaticsfinal.py
+++ informaticsfinal.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python
-"""Author: Andrew Wentz
+"""Authors: Andrew Wentz, Adrian Jania, Emmie Grimmie
 CSCE100 Final Project
 """
 import csv
+#open original data file
 file = open("reports.csv")
 csv_file = csv.reader(file)
+#open edited data file
+newfile = open("alcohols.csv")
+csv_newfile = csv.reader(newfile)
 
 dictionary = {
     
 }
 
 #location = input("Enter your current location: ")
-#time = input("Enter time: ")
-loclist = []
-count = 0
-for row in csv_file:
-    loclist.append(row[7])
-print(list(set(sorted(loclist))))
-    #if time in row:
-        #print(row)
-    #if location in row:
-        #count = count+1
-        #print(row)
-        #print("{}, {}, {}".format(row[1], row[2], row[10]))
-#print(count)
+
+#function for removing duplicate locations
+"""def locationparse():
+    loclist = []
+    count = 0
+    for row in csv_file:
+        loclist.append(row[7])
+    print(list(set(sorted(loclist))))"""
+    
+    #function for returning whole rows of info
+    """if location in row:
+        count = count+1
+        print("{}, {}, {}".format(row[1], row[2], row[10]))
+print(count)"""
